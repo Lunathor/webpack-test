9e74d815278eeed3a8178dbab099f1d80d7deae5
Lunathor <mrcoopgmz@gmail.com>
2025-06-12 17:42:32 +0300

    ES6 modules end, new type of information output

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-import {sum} from "./math";
+import * as math from "./math";
+const multiply = (a) => a * 8;
+const output = (text) => {
+  const div = document.createElement('div');
+  div.textContent = text;
+  document.body.appendChild(div);
+};
 
-console.log('ES6 modules')
-console.log('sum = ' + sum(2, 3))
+output('ES6 modules');
+output('sum = ' + math.sum(2, 3));
+output('multiply from math.js = ' + math.multiply(5))
+output('multiply from index.js = ' + multiply(5));
