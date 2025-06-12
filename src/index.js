2b63a6b6efa6920784029d34c7a4787ce374b5f1
Lunathor <mrcoopgmz@gmail.com>
2025-06-12 17:08:38 +0300

    ES6 modules init

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-document.write('hello, world! \n');
+import {sum} from "./math";
+
+console.log('ES6 modules')
+console.log('sum = ' + sum(2, 3))
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,17 @@
 module.exports = {
     entry: './src/index.js',
+    mode: 'development',
     output: {
         path: __dirname + '/dist',
         filename: 'main.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            }
+        ]
     }
 }
